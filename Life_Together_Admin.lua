cfd1a437e765be73f9b5659cb74313f61c8fab3f
ThatAnonymousCoder <75636857+dudeididntliterally@users.noreply.github.com>
2026-07-15 02:55:37 -0400

    Update Life_Together_Admin.lua

--- Life_Together_Admin.lua
+++ Life_Together_Admin.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/dudeididntliterally/Main/refs/heads/main/Experiences/13967668166.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/dudeididntliterally/Main/refs/heads/main/Menu.lua'))()
